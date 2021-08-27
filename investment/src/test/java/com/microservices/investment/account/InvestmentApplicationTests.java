d419031c234f2476d2d36f8195c775c89ec2d508
LVKIN <lucas.gonzaga@enext.com.br>
2021-08-27 19:16:07 -0300

    feat: two more services added

---
+++ investment/src/test/java/com/microservices/investment/account/InvestmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.investment.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
